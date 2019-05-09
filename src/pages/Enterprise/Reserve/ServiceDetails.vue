b455c01761bef92b470e24961c76bed0dfd2e6a4
syn <www.642173909.com@qq.com>
2019-05-09 17:12:25 +0800

    添加讲师详情

--- src/pages/Enterprise/Reserve/ServiceDetails.vue
+++ src/pages/Enterprise/Reserve/ServiceDetails.vue
@@ -32,6 +32,7 @@
               20000元<span class="span-position">2999元</span>
             </div>
           </div>
+          <div class="main-center-tab"></div>
         </div>
       </div>
     </div>
@@ -66,9 +67,10 @@ export default {
   .main .banner{margin-bottom: 0.1rem;}
   .main .banner img{width: 100%;height: 1.66rem;}
   .main-center{background:rgba(255,255,255,1);padding: 0 0.2rem;}
-  .main-center .main-center-title{border-bottom: 0.01rem solid #E8E8E8;padding: 0.2rem 0;}
+  .main-center .main-center-title{padding: 0.2rem 0;}
   .main-center .main-center-title .main-center-title-name .name{font-size:0.17rem;font-family:PingFangSC-Medium;font-weight:500;color:rgba(0,0,0,1);}
   .main-center .main-center-title .main-center-title-name .btn{width: 1rem;height: 0.28rem;background:rgba(249,91,64,1);border-radius:0.05rem;color: #fff;font-size:14px;font-family:PingFangSC-Regular;font-weight:400;line-height: 0.28rem;text-align: center;margin-left: 0.1rem;}
   .main-center-title-price{font-size:0.16rem;font-family:PingFangSC-Medium;font-weight:500;color:rgba(249,91,64,1);padding-top: 0.1rem}
   .main-center-title-price span{font-size:0.15rem;font-family:PingFangSC-Regular;font-weight:400;color:rgba(167,167,167,1);text-decoration: line-through;padding-left: 0.1rem;}
+  .main-center-tab{border-bottom: 0.01rem solid #E8E8E8;border-top: 0.01rem solid #E8E8E8;}
 </style>
